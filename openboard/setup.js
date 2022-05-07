1b98f47ccdd1dab66a4b88c6561cc13c6ed48eb3
sunil-01 <76584328+sunil-01@users.noreply.github.com>
2022-05-07 01:25:49 +0100

    Update setup.js

--- openboard/setup.js
+++ openboard/setup.js
@@ -1,7 +1,6 @@
 let cTool = "pencil";
 let canvasBoard = document.querySelector("canvas");
 let tool = canvasBoard.getContext("2d");
-// console.log(tool);
 let body = document.querySelector("body");
 // canvas dimesnions set karne ke baad jo changes karoge wahi reflect
 canvasBoard.height = window.innerHeight;
